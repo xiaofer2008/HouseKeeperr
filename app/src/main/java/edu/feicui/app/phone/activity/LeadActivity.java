a1b90e779d7408dcd866ad56bdfb719cc421a188
jxf <xiaofer2008@126.com>
2016-12-26 16:28:47 +0800

    add all files

---
+++ app/src/main/java/edu/feicui/app/phone/activity/LeadActivity.java
@@ -0,0 +1,16 @@
+package edu.feicui.app.phone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.feicui.app.phone.R;
+
+public class LeadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lead);
+    }
+}
